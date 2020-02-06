382ec69e3d0bb07fc82c53f034fd26389d903610
Radames Galvao <radamesh@gmail.com>
2020-02-06 11:28:18 -0300

    app-crud with form products

--- src/app/produto-novo/produto-novo.component.ts
+++ src/app/produto-novo/produto-novo.component.ts
@@ -8,19 +8,35 @@ import { FormBuilder, FormGroup, NgForm, Validators } from '@angular/forms'
   templateUrl: './produto-novo.component.html',
   styleUrls: ['./produto-novo.component.scss']
 })
+
 export class ProdutoNovoComponent implements OnInit {
 
-  productForm: FormGroup;
-  isLoadingResults = false;
+  productForm: FormGroup
+  prod_name: String = ''
+  prod_desc: String = ''
+  prod_price: String = ''
+  isLoadingResults = false
 
   constructor(private router: Router, private api: ApiService, private formBuilder: FormBuilder) { }
 
   ngOnInit() {
     this.productForm = this.formBuilder.group({
       'nome_produto' : [null, Validators.required],
-      'desc_produto' : [null, Validators.required, Validators.minLength(4)],
+      'desc_produto' : [null, Validators.required],
       'preco_produto': [null, Validators.required]
     });
   }
 
+  addProduto(form: NgForm) {
+    this.isLoadingResults = true
+    this.api.addProduto(form)
+      .subscribe(res => {
+        const id = res['_id']
+        this.isLoadingResults = false
+        this.router.navigate(['/produto-detalhe, id'])
+      }, (err) => {
+        console.log(err)
+        this.isLoadingResults = false
+      })
+  }
 }
