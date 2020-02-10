bd81b4064f3d25113e41a70a8856eefb527d2915
Radames Galvao <radamesh@gmail.com>
2020-02-09 22:50:43 -0300

    detalhes do produto

--- src/app/produto-detalhe/produto-detalhe.component.ts
+++ src/app/produto-detalhe/produto-detalhe.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ApiService } from '../api.service';
+import { Router, ActivatedRoute } from '@angular/router';
+import { Produto } from 'src/model/produto'
 @Component({
   selector: 'app-produto-detalhe',
   templateUrl: './produto-detalhe.component.html',
   styleUrls: ['./produto-detalhe.component.scss']
 })
 export class ProdutoDetalheComponent implements OnInit {
-
-  constructor() { }
+  produto: Produto = {_id: '', nome_produto: '', desc_produto: '', preco_produto: null, dt_atualizacao: null}
+  isLoadResults = true
+  constructor(private router: Router, private route: ActivatedRoute, private api: ApiService) { }
 
   ngOnInit() {
+    this.getProduto(this.route.snapshot.params['id'])
   }
 
 }
