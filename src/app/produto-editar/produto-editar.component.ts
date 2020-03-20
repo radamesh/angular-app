d9e31065cd0c7602b3561d7fea9dde3c3e5d999f
Radames Galvao <radamesh@gmail.com>
2020-03-19 21:37:50 -0300

    editar action controllers

--- src/app/produto-editar/produto-editar.component.ts
+++ src/app/produto-editar/produto-editar.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router, ActivatedRoute } from '@angular/router';
+import { FormBuilder, FormGroup, NgForm, Validators } from '@angular/forms'
+import { ApiService } from '../api.service';
 @Component({
   selector: 'app-produto-editar',
   templateUrl: './produto-editar.component.html',
   styleUrls: ['./produto-editar.component.scss']
 })
 export class ProdutoEditarComponent implements OnInit {
+  _id: String = ''
+  productForm: FormGroup
+  nome_produto: String = ''
+  desc_produto: String = ''
+  preco_produto: number = null
+  isLoadResults = false
 
-  constructor() { }
+  constructor(private router: Router, private route: ActivatedRoute, private api: ApiService, private formBuider: FormBuilder) { }
 
   ngOnInit() {
+    this.getProduto(this.route.snapshot.params['id'])
+    this.productForm = this.formBuider.group({
+      'nome_produto': [null, Validators.required],
+      'desc_produto': [null, Validators.required],
+      'preco_produto': [null, Validators.required]
+    })
   }
 
+  getProduto(id) {
+
+  }
+
+  updateProduto() {
+
+  }
 }
